3ef6a013bb51e628e04119e9615dcd5a66e08e53
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-02-01 06:57:34 +0900

    Initial Commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   string `json:"year"`
+}
+
+var books []Book
+
+func main() {
+
+}
