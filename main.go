a8602a17d8e3a6453942fba34e1a98b094a53202
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-02-02 07:02:15 +0900

    create the router and register api-endpoints and handler

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Book struct {
 	ID     int    `json:"id"`
 	Title  string `json:"title"`
@@ -10,5 +17,33 @@ type Book struct {
 var books []Book
 
 func main() {
+	router := mux.NewRouter()
 
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	router.HandleFunc("/books/{id}", getBook).Methods("GET")
+	router.HandleFunc("/books", addBook).Methods("POST")
+	router.HandleFunc("/books", updateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get all books is called")
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Get book is called")
+}
+
+func addBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Add book is called")
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Update book is called")
+}
+
+func removeBook(w http.ResponseWriter, r *http.Request) {
+	log.Println("Remove book is calleds")
 }
