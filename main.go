6445ad0029dc44cdd25baeeaf0c1abe324ff6fd8
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-02-02 07:08:11 +0900

    Add static data and implement getBooks

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -19,6 +20,14 @@ var books []Book
 func main() {
 	router := mux.NewRouter()
 
+	books = append(books,
+		Book{ID: 1, Title: "Golang pointers", Author: "Mr. Golang", Year: "2010"},
+		Book{ID: 2, Title: "Goroutines", Author: "Mr. Goroutine", Year: "2011"},
+		Book{ID: 3, Title: "Golang routers", Author: "Mr. ROuter", Year: "2012"},
+		Book{ID: 4, Title: "Golang concurrency", Author: "Mr. Currency", Year: "2013"},
+		Book{ID: 5, Title: "Golang good parts", Author: "Mr. Good", Year: "2014"},
+	)
+
 	router.HandleFunc("/books", getBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", getBook).Methods("GET")
 	router.HandleFunc("/books", addBook).Methods("POST")
@@ -29,7 +38,7 @@ func main() {
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
-	log.Println("Get all books is called")
+	json.NewEncoder(w).Encode(books)
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
