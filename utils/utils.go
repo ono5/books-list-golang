f504df74176d91587b5c911467b9985d879ba240
ono5 <kuroneko_head_lord@yahoo.co.jp>
2024-02-04 11:33:10 +0900

    Create utils methods

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,12 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+
+	models "github.com/ono5/books-list-golang/model"
 )
 
 func LogFatal(err error, comment string) {
@@ -12,4 +16,13 @@ func LogFatal(err error, comment string) {
 		log.Fatal(err)
 		fmt.Println("#############################################")
 	}
+}
+
+func SendError(w http.ResponseWriter, status int, err models.Error) {
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(err)
+}
+
+func SendSuccess(w http.ResponseWriter, data any) {
+	json.NewEncoder(w).Encode(data)
 }
